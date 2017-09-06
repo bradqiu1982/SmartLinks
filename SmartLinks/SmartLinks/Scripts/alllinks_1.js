f68cfd915f30fc73a91769a9c61ed9be4d4b322c
Yan Shi <yan.shi@finisar.com>
2017-09-06 12:46:26 +0800

    update smarklink html+css+js

--- SmartLinks/SmartLinks/Scripts/alllinks_1.js
+++ SmartLinks/SmartLinks/Scripts/alllinks_1.js
@@ -7,20 +7,33 @@ var lightBox = function(){
             for (var i = 1; i <= output.data.length; i++) {
                 var div_str = '<div class="div-title hide">' +
                             '<span class="glyphicon glyphicon-remove span-times delLink"' +
-                                'data-link-name="' + output.data[i].LinkName + '" aria-hidden="true" title="Delete"></span>' +
+                                'data-link-name="' + output.data[i-1].LinkName + '" aria-hidden="true" title="Delete"></span>' +
                             '</div>'+
-                            '<span class="bg-default-data-transparent" data-name="' + output.data[i].LinkName + '">&nbsp;</span>';
-                $('#link' + i).removeClass('bg-default-link').addClass('bg-default-data');
+                            '<span class="bg-default-data-transparent" data-name="' + output.data[i - 1].LinkName + '">&nbsp;</span>';
                 $('#link' + i).append(div_str);
+                if ($('#link' + i).hasClass('bg-default-link')) {
+                    $('#link' + i).removeClass('bg-default-link').addClass('bg-default-data');
+                }
+                if ($('#link' + i).hasClass('bg-default-link-rect')) {
+                    $('#link' + i).removeClass('bg-default-link-rect').addClass('bg-default-data-rect');
+                    $('#link' + i).children('span').removeClass('bg-default-data-transparent').addClass('bg-default-data-transparent-rect');
+                }
             }
-            $.each(function () {
-
+            $('.bg-default-link').each(function () {
+                $(this).append(
+                    '<span class="bg-default-link-transparent">&nbsp;</span>'
+                );
             })
+            $('.bg-default-etc').append('<span class="bg-default-link-transparent">&nbsp;</span>');
         });
         
         $('body').on('click', '.bg-default-data-transparent', function () {
             var link_name = $(this).attr('data-name');
-            alert(link_name);
+            window.location.href = '/SmartLinks/RedirectToLink?linkname=' + link_name;
+        })
+
+        $('body').on('click', '.bg-default-data-transparent-rect', function () {
+            var link_name = $(this).attr('data-name');
             window.location.href = '/SmartLinks/RedirectToLink?linkname=' + link_name;
         })
 
@@ -54,7 +67,7 @@ var lightBox = function(){
             var comment = $('#comment').val();
             var image_url = $('#imgurl').val();
             if (!link_name || !link) {
-                return false;
+                alert('Please input LinkName and Link !');
             }
 
             $.post('/SmartLinks/AddCustomLink',
