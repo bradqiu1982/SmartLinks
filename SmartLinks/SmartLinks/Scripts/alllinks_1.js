39f6256b52a859513dd2b2602fe574de563eb8db
Brad Qiu <brad.qiu@finisar.com>
2018-03-16 15:06:35 +0800

    add video icon for technical video link

--- SmartLinks/SmartLinks/Scripts/alllinks_1.js
+++ SmartLinks/SmartLinks/Scripts/alllinks_1.js
@@ -11,10 +11,10 @@ var lightBox = function () {
         var default_imgs_ie8 = new Array();
         var default_links_ie8 = new Array();
         default_imgs = ['./Content/images/peace.png', './Content/images/data.png', './Content/images/daisy.png'
-            , './Content/images/clover.png', './Content/images/scrap.png', './Content/images/component.png'];
+            , './Content/images/clover.png', './Content/images/scrap.png', './Content/images/component.png', './Content/images/video.png'];
         default_imgs_ie8 = ['./Content/images/ie8/peace.png', './Content/images/ie8/data.png', './Content/images/ie8/daisy.png'
-            , './Content/images/ie8/clover.png', './Content/images/ie8/scrap.png', './Content/images/ie8/component.png'];
-        default_links_ie8 = ['AGILE', 'BR', 'DOMINO', 'ERP', 'FA', 'TRACEVIEW', 'DAISY', 'OA', 'CLOVER', 'SCRAP', 'COMPONENT'];
+            , './Content/images/ie8/clover.png', './Content/images/ie8/scrap.png', './Content/images/ie8/component.png', './Content/images/ie8/video.png'];
+        default_links_ie8 = ['AGILE', 'BR', 'DOMINO', 'ERP', 'FA', 'TRACEVIEW', 'DAISY', 'OA', 'CLOVER', 'SCRAP', 'COMPONENT','VIDEO'];
 
         $.post('/SmartLinks/AllData',
         {
